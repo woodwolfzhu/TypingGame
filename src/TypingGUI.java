ffdaab9ea07ee5ca69deec423e5b2daa924d0765
woodwolfzhu <woodwolfzhu@gmail.com>
2019-07-03 21:55:54 +0800

    1.主界面、游戏界面、暂停界面暂时设置好了 2.陨石类还需要更改 3.游戏数据还没有处理

--- src/TypingGUI.java
+++ src/TypingGUI.java
@@ -1,12 +1,9 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TypingGUI {
+public class TypingGUI implements ActionListener{
     JFrame frame;
     JPanel jPanel;
     Graphics g;
@@ -20,20 +17,29 @@ public class TypingGUI {
     JLabel pauseLabel;      // 暂停时的提示语
     JButton resumeButton;         // 继续按钮
     JLabel mshipLabel;      // 主界面小飞船
-//    JLabel gshipLabel;      // 游戏界面飞船
-    Meteorites meteorites;
-
+    //    JLabel gshipLabel;      // 游戏界面飞船
+    Meteorites[] min;     // 小陨石
+    Meteorites[] mid;     // 中陨石
+    Meteorites[] larg;     // 大陨石
+    int checkPoint;             // 关卡
+    int shipBlood;        // 飞船的血，暂时默认为 1；被撞后游戏结束
+    String[] text;        // 陨石上的单词
+    int wordNum;          // 记录text中未使用的单词的位置
 
     TypingGUI() {
         frame = new JFrame("Typing Game");
+        wordNum =0;
 
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 800);
         frame.setLocation(500, 200);
         g = frame.getGraphics();
+        shipBlood = 1;
+        initButton();
         mainGUI();    // 设置主界面背景图
-        gamingGUI();
+//        pauseGUI();
+//        gamingGUI();
         frame.setResizable(false);  // 禁止改变窗口大小
         frame.setVisible(true);
 
@@ -46,31 +52,31 @@ public class TypingGUI {
         backGroundLabel.setBounds(0, 0, 600, 800);
         ((JPanel) frame.getContentPane()).setOpaque(false); // 将内容面板设置为透明
 //        try {
-//            File file = new File(this.getClass().getResource("/res/Snipaste_2019-03-17_09-45-02.png").getPath() );
+//            File file = new File("res/1.png" );
 //            FileInputStream fis = new FileInputStream(file);
 //            BufferedImage im = new BufferedImage(600,800,BufferedImage.TYPE_INT_ARGB);
-
-//            getClass().getResource("res/Snipaste_2019-03-17_09-45-02.png");
-
-//            im = ImageIO.read(getClass().getResource("res/Snipaste_2019-03-17_09-45-02.png"));
-////            if(file.exists()){
-////                System.out.println("存在a");
-////            }
+//
+//            im = ImageIO.read(fis);
+//            if(file.exists()){
+//                System.out.println("存在a");
+//            }
 //            g.drawImage(im,0,0,null);
 
-            label = new JLabel("Typing");       // 游戏大标题
-            label.setFont(getFont(80));
-            label.setForeground(Color.WHITE);
-            label.setBounds(170, 100, 280, 200);
-            frame.add(label);
 
-            setButton();    // 对主界面按钮进行设置
 
-            //主界面小飞船
-            ImageIcon imageIcon1 = new ImageIcon("res/ship.png");
-            mshipLabel = new JLabel(imageIcon1);
-            mshipLabel.setBounds(300, 350, 23, 297);
-            frame.add(mshipLabel);
+        label = new JLabel("Typing");       // 游戏大标题
+        label.setFont(getFont(80));
+        label.setForeground(Color.WHITE);
+        label.setBounds(170, 100, 280, 200);
+        frame.add(label);
+
+        setButton();    // 对主界面按钮进行设置
+
+        //主界面小飞船
+        ImageIcon imageIcon1 = new ImageIcon("res/ship.png");
+        mshipLabel = new JLabel(imageIcon1);
+        mshipLabel.setBounds(300, 350, 23, 297);
+        frame.add(mshipLabel);
 //        }catch(IOException e){
 //            System.out.println(e.getMessage());
 //        }
@@ -78,14 +84,12 @@ public class TypingGUI {
     }
 
     void gamingGUI() {  // 游戏时的画面
+
         frame.remove(newGameButton);
         frame.remove(loadTextButton);
         frame.remove(label);
-        mshipLabel.setBounds(290,700,23,297);
-        ImageIcon imageIcon = new ImageIcon("res/pause.png");
-        pauseButton = new JButton(imageIcon);
-        pauseButton = setButtonOpaque(pauseButton); // 设置为只显示图片
         pauseButton.setBounds(20, 20, 18, 21);
+        mshipLabel.setBounds(290, 700, 23, 297);
 
         frame.add(pauseButton);
         frame.setVisible(true);
@@ -96,22 +100,39 @@ public class TypingGUI {
         pauseLabel.setFont(getFont(50));
         pauseLabel.setForeground(Color.WHITE);
         pauseLabel.setBounds(100, 100, 500, 200);
-        resumeButton = new JButton("继续");
+
         resumeButton.setFont(getFont(40));
         resumeButton.setForeground(Color.WHITE);
         resumeButton = setButtonOpaque(resumeButton);
-        resumeButton.setBounds(200,550,200,80);
+        resumeButton.setBounds(200, 550, 200, 80);
         frame.add(pauseLabel);
         frame.add(resumeButton);
     }
 
-    void paint(){   // 实现对陨石的绘制
+    void endGUI() { // 结束画面
+    }
+
+
+    void initButton(){
+
+        newGameButton = new JButton("new game");
+        loadTextButton = new JButton("load text");
+        resumeButton = new JButton("继续");
+        ImageIcon imageIcon = new ImageIcon("res/pause.png");
+        pauseButton = new JButton(imageIcon);
+        pauseButton = setButtonOpaque(pauseButton); // 设置为只显示图片
+
+        newGameButton.addActionListener(this);
+        loadTextButton.addActionListener(this);
+        pauseButton.addActionListener(this);
+        resumeButton.addActionListener(this);
+
 
     }
 
+
     void setButton() {  // 将主界面按钮进行设置
-        newGameButton = new JButton("new game");
-        loadTextButton = new JButton("load text");
+
 
         newGameButton = setButtonOpaque(newGameButton); // 设置为只显示图片或文字
         loadTextButton = setButtonOpaque(loadTextButton);
@@ -123,15 +144,80 @@ public class TypingGUI {
 
         newGameButton.setBounds(new Rectangle(200, 450, 200, 80));
         loadTextButton.setBounds(new Rectangle(200, 550, 200, 80));
+
         frame.add(newGameButton);
         frame.add(loadTextButton);
+
     }
 
-    void  endGUI(){ // 结束画面
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newGameButton) {
+            gamingGUI();
+            makeMeteorites();
+        } else if (e.getSource() == loadTextButton) {
+            loadText();
+        } else if (e.getSource() == pauseButton) {
+            pauseGUI();
+        } else if (e.getSource() == resumeButton) {
 
+            frame.remove(pauseLabel);
+            frame.remove(resumeButton);
+            gamingGUI();
+        }
+        frame.repaint();
+    }
+
+    void loadText() { // 从TextArea 中获得字符串并赋给gui.text;
+
+    }
+
+
+
+    void makeMeteorites() {
+        int i = checkPoint;
+        int sum = 0;
+        int min, mid, larg;
+        mid = i / 3;   // 中型陨石每3关增加一个
+        larg = i / 5;  // 大陨石每5关增加一个
+        if (i % 3 != 0 && i % 5 != 0) {   // 小陨石在这些特定关卡中不增加
+            min = i + 4;
+        } else {
+            min = i + 3;
+        }
+        sum = min + mid + larg;
+
+        this.min = new miniMeteorites[min];
+        this.mid = new midMeteorites[mid];
+        this.larg = new largMeteorites[larg];
+
+        int x;
+        for (x = 0; x < min; x++) {
+            this.min[x] = new miniMeteorites();
+            this.min[x].setWord(text[wordNum%text.length]);
+            frame.add(this.min[x]);
+            wordNum++;
+        }
+        for (x = 0; x < mid; x++) {
+            this.mid[x] = new midMeteorites();
+            this.mid[x].setWord(text[wordNum%text.length]);
+            frame.add(this.mid[x].imageLabel);
+            wordNum++;
+        }
+        for (x = 0; x < larg; x++) {
+            this.larg[x] = new largMeteorites();
+            this.larg[x].setWord(text[wordNum%text.length]);
+            frame.add(this.larg[x]);
+            wordNum++;
+        }
+    }
+
+    void setText(String[] text){   // 给text 赋值
+        this.text = text;
     }
 
     JButton setButtonOpaque(JButton button) {    // 将按钮设置为只显示图片
+
         button.setBorderPainted(false); // 不打印边框
         button.setBorder(null);  // 除去边框
         button.setFocusPainted(false);   // 除去焦点的框
@@ -144,3 +230,4 @@ public class TypingGUI {
         return font;
     }
 }
+
--- src/meteorites.java
+++ src/meteorites.java
@@ -1,22 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.util.Random;
 
-class Meteorites {
+class Meteorites extends JPanel{
     ImageIcon image;
     JLabel imageLabel;
     JLabel word;
-    int time; // 下降的速度
-    JFrame frame;
-    int x, y; // 陨石的坐标
-    int tx, ty; // 陨石出现的位置与飞船之间的坐标差，
+    private int time; // 下降的速度
+    private int x, y; // 陨石的坐标
+    private int tx, ty; // 陨石出现的位置与飞船之间的坐标差，
+    private int blood;  // 陨石的“血量”，大小与单词的长度一致，每击中一次减一
+    Graphics g;
+    String text1; // 游戏使用的文本文件
+    String[] text2; // 处理后的文本，去掉空格和各种标点
 
-    Meteorites(JFrame frame) {
-        this.frame = frame;
+    Meteorites() {
+        setSpeed();
+        g= this.getGraphics();
+        this.setOpaque(false);   // 将面板设置为透明
+        this.add(imageLabel);
+        this.add(word);
+        initText();
+        dealText();
     }
 
-    void setImage() {// 设置显示图片
-    }
 
     void setWord(String word) {// 设置显示文字
         this.word.setText(word);
@@ -27,42 +35,85 @@ class Meteorites {
 //        this.word.setOpaque(true); // 试试label的透明是什么效果
     }
 
-    void setSpeed(int time) {// 设置下降速度
+    void setSpeed() {// 设置下降速度
         Random rd = new Random();
         x = rd.nextInt(600); // 陨石从最上面的随机位置出现
         y = 0;
-        tx = x - 290;
-        ty = 700;
-        this.time = time;
+        tx = 1;
+        ty = 1;
     }
 
-    void setLocation(Graphics g) {  // 设置位置
-        imageLabel.setLocation(x,y);
-        word.setLocation(x+32,y+32);
-        x= x+tx/time;
-        y=y+ty/time;
+    void setLocation() {  // 设置位置的移动
+        imageLabel.setLocation(x, y);
+        word.setLocation(x + 32, y + 32);
+        if (y != 290) {
+            x = x + tx;
+        }
+        y = y + ty;
+    }
+
+    void setBlood() {
+        blood = blood - 1;
+    }
+
+    int getBlood() {
+        return blood;
     }
 
     Font getFont(int x) {        // 获得字体大小为 x 的 font 对象
         Font font = new Font("宋体", Font.BOLD, x);
         return font;
     }
+
+
+    class PaintThread extends Thread {   // 1 个线程只能控制一个陨石
+        boolean flag = true;  // 控制线程进行
+        Meteorites meteorites;
+
+        public void run() {
+            while (true) {
+                setLocation();
+                if (meteorites.getBlood() == 0) { // 陨石血量为 0 ，或者飞船被撞毁，线程结束
+                    flag = false;
+                }
+            }
+        }
+    }
+
+    void initText() {
+        try {
+            File f = new File("res/text.txt");
+            FileInputStream fis = new FileInputStream(f);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                text1 = text1 + str;
+            }
+            dealText();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    void dealText(){
+        text2 = text1.split("[^a-z]"); // 获得干净的单词
+    }
 }
 
 
 class miniMeteorites extends Meteorites {
-    miniMeteorites(JFrame frame) {
-        super(frame);
+    miniMeteorites() {
         image = new ImageIcon("res/mine.png");
         imageLabel = new JLabel(image);
         word = new JLabel();
+
     }// 小型陨石
 }
 
 
 class midMeteorites extends Meteorites {
-    midMeteorites(JFrame frame) {
-        super(frame);
+    midMeteorites() {
         image = new ImageIcon("res/destroyer.png");
         imageLabel = new JLabel(image);
         word = new JLabel();
@@ -70,8 +121,7 @@ class midMeteorites extends Meteorites {
 }
 
 class largMeteorites extends Meteorites {
-    largMeteorites(JFrame frame) {
-        super(frame);
+    largMeteorites() {
         image = new ImageIcon("res/oppressor.png");
         imageLabel = new JLabel(image);
         word = new JLabel();
